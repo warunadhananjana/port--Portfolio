98da0892870eb47ad51fdc10bf0481cd65fcfb90
dhananjana123 <warunad999@gmail.com>
2024-12-09 14:53:05 +0530

    profle image change

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,18 +10,18 @@ const container = (delay) => ({
     transition: { duration: 0.5, delay: delay },
   },
 });
+
 function Hero() {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
-      <div className="flex flex-wrap ">
-        <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start ">
+      <div className="flex flex-wrap lg:flex-nowrap">
+        <div className="w-full lg:w-1/2 order-2 lg:order-1">
+          <div className="flex flex-col items-center lg:items-start">
             <motion.h1
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-thin tracking-tight lg:-16
-            lg:text-8xl "
+              className="pb-16 text-6xl font-thin tracking-tight lg:mb-16 lg:text-8xl"
             >
               Waruna Dhananjana
             </motion.h1>
@@ -48,8 +48,8 @@ function Hero() {
             </motion.p>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
-          <div className="flex  justify-center ">
+        <div className="w-full lg:w-1/2 order-1 lg:order-2 lg:p-8">
+          <div className="flex justify-center">
             <motion.img
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
