f8508fc93275338ac3e6a3ff7adf62bde184c80c
dhananjana123 <warunad999@gmail.com>
2024-12-09 14:58:22 +0530

    remove contact animation

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,34 +1,13 @@
 import React from "react";
 import { CONTACT } from "../constants/index";
-import { motion } from "motion/react";
+
 function Contact() {
   return (
     <div className="border-b border-neutral-900">
-      <motion.h1
-        whileInView={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: -100 }}
-        transition={{ duration: 0.5 }}
-        className="my-10 text-center text-4xl font-bold"
-      >
-        Get in Touch
-      </motion.h1>
+      <h1 className="my-10 text-center text-4xl font-bold">Get in Touch</h1>
       <div className="text-center tracking-tighter">
-        <motion.p
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1 }}
-          className="my-4"
-        >
-          {CONTACT.address}
-        </motion.p>
-        <motion.p
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1 }}
-          className="my-4"
-        >
-          {CONTACT.phoneNo}
-        </motion.p>
+        <p className="my-4">{CONTACT.address}</p>
+        <p className="my-4">{CONTACT.phoneNo}</p>
         <a href="#" className="border-b">
           {CONTACT.email}
         </a>
