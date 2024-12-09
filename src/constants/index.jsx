bca187a73d51d54e2a90635f0810e1ea593f1d75
dhananjana123 <warunad999@gmail.com>
2024-12-09 10:31:44 +0530

    remove items

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -7,37 +7,6 @@ export const HERO_CONTENT = `I am a passionate full stack developer with a knack
 
 export const ABOUT_TEXT = `I am Waruna Dhananjana, a passionate software engineering student at the Faculty of Computing, Sabaragamuwa University of Sri Lanka. With a strong foundation in front-end development, I specialize in crafting visually appealing and user-friendly interfaces. I enjoy working with modern technologies like React, Node.js, and Next.js, constantly exploring innovative ways to enhance user experiences. . My journey is fueled by a commitment to lifelong learning and the pursuit of excellence in the tech industry.`;
 
-export const Education = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
-
 export const PROJECTS = [
   {
     title: "MedExplore (Group)",
